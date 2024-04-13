1f018d1c7861ea74fc2a85b7aaa5041a34f8be74
Rajan Prajapati <rajanprajapati743@gmail.com>
2024-04-13 22:45:12 +0530

    db connected

---
+++ src/index.js
@@ -0,0 +1,39 @@
+// require('dotenv').config()
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path:'/.env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+/*
+
+import express from 'express'
+const app = express()
+
+;(async () => {
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)    
+       app.on("error",(error)=>{
+        console.log("Some error for tailking",error);
+        throw error
+       })    
+       app.listen(process.env.PORT,()=>{
+        console.log(`App is listining on ${process.env.PORT}` );
+       })
+    } catch (error) {
+        console.log("error",error);
+    }
+})();
+*/
